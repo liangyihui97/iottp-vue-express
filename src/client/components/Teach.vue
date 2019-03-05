1ced43effddc562057533927a6c5e63293d32af3
liangyihui97 <liangyihui97@163.com>
2019-03-05 11:58:24 +0800

    更新教学部分nav

--- src/client/components/Teach.vue
+++ src/client/components/Teach.vue
@@ -1,22 +1,47 @@
 <template>
-  <div>
-    <h1>{{ msg }}</h1>
-   {{data}}{{columns}}{{data2}}
-  </div>
+<div>
+    <div id="teach">
+        <div class="teach-nav">
+            <div class="nav-class">
+                <myNav v-model="content">
+
+                </myNav>
+            </div>
+        </div>
+        <div class="teach-page">
+
+        </div>
+    </div>
+</div>
 </template>
 
 <script>
+import myNav from './nav'
 export default {
+components:{myNav},
   data () {
     return {
       // note: changing this line won't causes changes
       // with hot-reload because the reloaded component
       // preserves its current state and we are modifying
       // its initial state.
-      msg: 'this is teach!',
      columns: [],
       data: [],
       data2: [],
+      content:[
+                {
+                    content: '仪表盘',
+                    img: require('../static/images/01nav.png')
+                },
+                {
+                    content:'财产管理',
+                    img: require('../static/images/02nav.png')
+                },
+                {
+                    content:'教学内容',
+                    img: require('../static/images/03nav.png')
+                }
+                ]
     }
   },
  mounted (){
@@ -25,12 +50,12 @@ export default {
 	self.data = response.data.data;
 	self.columns = response.data.columns;
 });
-this.$axios.get('/v2/music/search?q=周杰伦',).then(function (response) {
-        self.data2 = response.data.musics;
-    })
-    .catch(function (error) {
-        console.log(error);
-    })
+//this.$axios.get('/v2/music/search?q=周杰伦',).then(function (response) {
+//        self.data2 = response.data.musics;
+//    })
+  //  .catch(function (error) {
+    //    console.log(error);
+   // })
 },
 }
 </script>
---
+++ src/client/components/nav.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="nav">
+            <div
+            v-for="(item,index) in value"
+            :class="navCls(index)"
+            @click="handleChange(index)">
+            <img :src="item.img" height="25" width="25" class="fl">
+            <div class="nav-text fl">{{item.content}}</div>
+            </div>
+            </div>
+</template>
+<script>
+export default{
+          props:['value'],
+        data: function(){
+            return {
+                currentValue: ''
+            }
+        },
+        methods:{
+            handleChange: function (index) {
+                this.currentValue = index
+            },
+            navCls: function (index) {
+                //console.log(index);
+                return[
+                    'nav-content',
+                    index.toString(),
+                    {
+                        'nav-active':index === this.currentValue
+                    }
+                ]
+            }
+        }
+}
+</script>
