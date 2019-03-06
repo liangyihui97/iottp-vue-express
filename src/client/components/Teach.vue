b2b25b891a61d27b0f3c3d1a99fd061b2aac90f8
liangyihui97 <liangyihui97@163.com>
2019-03-06 12:05:34 +0800

    更新教学部分page

--- src/client/components/Teach.vue
+++ src/client/components/Teach.vue
@@ -1,18 +1,53 @@
 <template>
 <div>
-    <div id="teach">
-        <div class="teach-nav">
-                <my-nav v-model="content">
+<div id="teach">
+                <div class="teach-nav">
+                    <my-nav v-model="content">
 
-                </my-nav>
-        </div>
-        <div class="teach-page">
-            <div class="teach-title">
-                <div class="teach-title-txt fl">实训1：门磁传感器ZigBee无线网络实训</div>
-                <div class="teach-user fr"></div>
+                    </my-nav>
+                </div>
+                <div class="teach-page">
+                    <div class="teach-title">
+                        <div class="teach-select " v-clickoutside="handleClose"@mouseover="show=true">
+                            <img src="../static/images/select.png" width="34" height="34" alt="选择课程" title="选择课程">
+                            <div class="teach-select-text fr">选择课程</div>
+                            <div class="dropdown" v-show="show">
+                                <p><a href="#">实训1</a></p>
+                                <p><a href="#">实训2</a></p>
+                                <p><a href="#">实训3</a></p>
+                                <p><a href="#">实训4</a></p>
+                                <p><a href="#">实训5</a></p>
+                                <p><a href="#">实训6</a></p>
+                                <p><a href="#">实训7</a></p>
+                                <p><a href="#">实训8</a></p>
+                                <p><a href="#">实训9</a></p>
+                                <p><a href="#">实训10</a></p>
+                                <p><a href="#">实训11</a></p>
+                                <p><a href="#">实训12</a></p>
+                                <p><a href="#">实训13</a></p>
+                                <p><a href="#">实训14</a></p>
+                            </div>
+                        </div>
+                        <div class="teach-title-txt ">实训1：门磁传感器ZigBee无线网络实训</div>
+                        <div class="teach-user fr"></div>
+                    </div>
+			<div class="teach-console">
+                        <div class="teach-console-title">
+                            拓扑界面
+                        </div>
+                    </div>
+                    <div class="teach-structure">
+                        <div class="teach-structure-title">
+                            网络结构
+                        </div>
+                    </div>
+                    <div class="teach-data">
+                        <div class="teach-data-title">
+                            设备检测
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
 </div>
 </template>
 
@@ -43,10 +78,11 @@ components:{myNav},
                     img: require('../static/images/03nav.png'),
 		    texts:['基础实训模块','智能家居实训模块','工业级实训模块']
                 }
-                ]
+                ],
+      show: false
     }
   },
- mounted (){
+    mounted (){
     var self = this;
     this.$axios.get('http://192.168.227.10:4000/api/data').then(function(response){
 	self.data = response.data.data;
@@ -59,5 +95,30 @@ components:{myNav},
     //    console.log(error);
    // })
 },
+   directives: {
+       clickoutside:{
+	    bind: function (el, binding, vnode) {
+            function documentHandler(e) {
+                if(el.contains(e.target)){
+                    return false;
+                }
+                if(binding.expression){//判断的是当前的指令 v-clickoutside 有没有写表达式，
+                    binding.value(e);//用来执行当前上下文 methods 中指定的函数。
+                }
+            }
+            el.__vueClickOutside__=documentHandler;//documentHandler的引用
+            document.addEventListener('mouseover',documentHandler);
+        },
+        unbind: function (el, binding, vnode) {
+            document.removeEventListener('mouseover',el.__vueClickOutside__);//因为自定义指令中不能用this.documentHandler
+            delete el.__vueClickOutside__;
+        }	
+	} 
+   },
+    methods:{
+        handleClose: function () {
+                this.show = false;
+            }
+    }
 }
 </script>
