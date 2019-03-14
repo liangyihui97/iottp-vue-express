2dd0dfb9e1d9afbe8130ce6a5dce386c93982bad
liangyihui97 <liangyihui97@163.com>
2019-03-14 14:21:23 +0800

    add teach-info

--- src/client/components/Teach.vue
+++ src/client/components/Teach.vue
@@ -1,105 +1,171 @@
 <template>
-<div class="main">
-<div style="height: 500px;" id="canvas"></div>
-<qunee-js src="./js/qunee.js">
-</qunee-js>
-<div id="teach" v-cloak>
-                <div class="teach-nav">
-                    <div class="teach-logo fl" @click="goIndex">
-                        <div class="teach-logo-a" title="物联网教学" >
-                            <img  class="fl" src="../static/images/logo.png" width="80" height="80">
-                            <div class="fl teach-logo-font">
-                                <span class="teach-logo-font-strong"><strong>物联网教学</strong></span><br/>
-                                <span style="font-size: 10px">IoT Teaching Platform</span>
-                            </div>
-                        </div>
-                    </div>
-                    <my-nav v-model="content" class="fl">
+<div class="main"> 
+   <div style="height: 500px;" id="canvas"></div> 
+   <qunee-js src="./js/qunee.js"> 
+   </qunee-js> 
+   <div id="teach" v-cloak=""> 
+    <div class="teach-nav"> 
+     <div class="teach-logo fl" @click="goIndex"> 
+      <div class="teach-logo-a" title="物联网教学"> 
+       <img class="fl" src="../static/images/logo.png" width="80" height="80" /> 
+       <div class="fl teach-logo-font"> 
+        <span class="teach-logo-font-strong"><strong>物联网教学</strong></span>
+        <br /> 
+        <span style="font-size: 10px">IoT Teaching Platform</span> 
+       </div> 
+      </div> 
+     </div> 
+     <my-nav v-model="content" class="fl"> 
+     </my-nav> 
+    </div> 
+    <div class="teach-page"> 
+     <div class="title "> 
+      <div class="teach-select " v-clickoutside="handleClose" @mouseover="show=true"> 
+       <img src="../static/images/select.png" width="34" height="34" alt="选择课程" title="选择课程" /> 
+       <div class="teach-select-text fr">
+        选择课程
+       </div> 
+       <div class="dropdown" v-show="show"> 
+        <p><a href="#">实训1</a></p> 
+        <p><a href="#">实训2</a></p> 
+        <p><a href="#">实训3</a></p> 
+        <p><a href="#">实训4</a></p> 
+        <p><a href="#">实训5</a></p> 
+        <p><a href="#">实训6</a></p> 
+        <p><a href="#">实训7</a></p> 
+        <p><a href="#">实训8</a></p> 
+        <p><a href="#">实训9</a></p> 
+        <p><a href="#">实训10</a></p> 
+        <p><a href="#">实训11</a></p> 
+        <p><a href="#">实训12</a></p> 
+        <p><a href="#">实训13</a></p> 
+        <p><a href="#">实训14</a></p> 
+       </div> 
+      </div> 
+      <div class="teach-title-txt ">
+       <strong>实训1：门磁传感器ZigBee无线网络实训</strong>
+      </div> 
+      <div class="teach-user fr"></div> 
+     </div> 
+     <div class="teach-console"> 
+      <div class="teach-title">
+        拓扑界面 
+      </div> 
+      <div class="teach-console-test"> 
+       <a href="#" @click="getData">连线完成，点击测试</a>
+       <img class="fr" src="../static/images/test.png" width="20px" height="auto" /> 
+      </div> 
+      <div class="teach-console-submit"> 
+       <a href="#">测试完成，提交结果</a>
+       <img class="fr" src="../static/images/submit.png" width="20px" height="auto" /> 
+      </div> 
+     </div> 
+     <div class="teach-structure"> 
+      <div class="teach-title">
+        网络结构 
+      </div> 
+      <div class="teach-structure-network"> 
+       <img src="../static/images/network.png" width="450" height="auto" /> 
+      </div> 
+     </div> 
+     <div class="teach-data"> 
+      <div class="teach-title">
+        设备检测 
+      </div> 
+      <my-data v-model="apiData"> 
+      </my-data> 
+     </div> 
+     <div class="teach-chart"> 
+      <div class="teach-title">
+        可视化 
+      </div> 
+      <div class="mychart fl" id="myChart" :style="{width: '550px', height: '300px'}"></div> 
+      <div class="mychart fl" id="myChart2" :style="{width: '550px', height: '300px'}"></div> 
+     </div> 
+     <div class="teach-info"> 
+      <div class="teach-title">
+        产品说明 
+      </div> 
+     <div class="teach-info-content">
+      <table border="1" class="gridtable"> 
+       <tbody>
+        <tr> 
+         <td>产品型号 </td> 
+         <td>HS1GW智能网关</td> 
+         <td>工作电压 </td> 
+         <td>AC100V~240V</td> 
+        </tr> 
+        <tr> 
+         <td>报警电流 </td> 
+         <td>≤30mA</td> 
+         <td>联网方式 </td> 
+         <td>ZigBee自组网</td> 
+        </tr> 
+        <tr> 
+         <td>无线组网距离 </td> 
+         <td>≤100米(空旷环境)</td> 
+         <td>工作温度 </td> 
+         <td>-10℃~+50℃</td> 
+        </tr> 
+        <tr> 
+         <td>环境湿度 </td> 
+         <td>最大95%RH</td> 
+         <td>A 外形尺寸 </td> 
+         <td>122*122*52mm</td> 
+        </tr> 
+        <tr> 
+         <td>B 产品尺寸 </td> 
+         <td>70*70*53mm</td>
+         <td></td> 
+         <td></td> 
+        </tr>
+       </tbody>
+      </table>
+<table border="1" class="gridtable">
+    <tr>
+        <td>产品型号	</td>
+        <td>HS1DS智能门磁探测器</td>
+        <td>工作电压	</td>
+        <td>DC3V</td>
+    </tr>
+    <tr>
+        <td>探测角度	</td>
+        <td>110º</td>
+        <td>探测距离	</td>
+        <td>≥20mm</td>
+    </tr>
+    <tr>
+        <td>报警电流	</td>
+        <td>≤5uA</td>
+        <td>联网方式	</td>
+        <td>ZigBee自组网</td>
+    </tr>
 
-                    </my-nav>
-                </div>
-		<div class="teach-page">
-                    <div class="title ">
-                        <div class="teach-select " v-clickoutside="handleClose"@mouseover="show=true">
-                            <img src="../static/images/select.png" width="34" height="34" alt="选择课程" title="选择课程">
-                            <div class="teach-select-text fr">选择课程</div>
-                            <div class="dropdown" v-show="show">
-                                <p><a href="#">实训1</a></p>
-                                <p><a href="#">实训2</a></p>
-                                <p><a href="#">实训3</a></p>
-                                <p><a href="#">实训4</a></p>
-                                <p><a href="#">实训5</a></p>
-                                <p><a href="#">实训6</a></p>
-                                <p><a href="#">实训7</a></p>
-                                <p><a href="#">实训8</a></p>
-                                <p><a href="#">实训9</a></p>
-                                <p><a href="#">实训10</a></p>
-                                <p><a href="#">实训11</a></p>
-                                <p><a href="#">实训12</a></p>
-                                <p><a href="#">实训13</a></p>
-                                <p><a href="#">实训14</a></p>
-                            </div>
-                        </div>
-                        <div class="teach-title-txt "><strong>实训1：门磁传感器ZigBee无线网络实训</strong></div>
-                        <div class="teach-user fr"></div>
-                    </div>
-                    <div class="teach-console">
-                        <div class="teach-title">
-                            拓扑界面
-                        </div>
+    <tr>
+        <td>无线组网距离	</td>
+        <td>≤100米(空旷环境)</td>
+        <td>工作温度	</td>
+        <td>-10℃~+50℃</td>
+    </tr>
 
-                        <div class="teach-console-test">
-                            <a href="#" @click="getData">连线完成，点击测试</a><img class="fr" src="../static/images/test.png" width="20px" height="auto">
-                        </div>
-                        <div class="teach-console-submit">
-                            <a href="#">测试完成，提交结果</a><img class="fr" src="../static/images/submit.png" width="20px" height="auto">
-                        </div>
-                    </div>
-                    <div class="teach-structure">
-                        <div class="teach-title">
-                            网络结构
-                        </div>
-                        <div class="teach-structure-network">
-                            <img src="../static/images/network.png" width="450"height="auto">
-                        </div>
-                    </div>
-                    <div class="teach-data">
-                        <div class="teach-title">
-                            设备检测
-                        </div>
-                        <my-data v-model="apiData">
-                        </my-data>
-                    </div>
-                    <div class="teach-chart">
-                        <div class="teach-title">
-                            可视化
-                        </div>
-                        <div class="mychart fl" id="myChart" :style="{width: '550px', height: '300px'}"></div>
-			<div class="mychart fl" id="myChart2" :style="{width: '550px', height: '300px'}"></div>
-                    </div>
-		    <div class="teach-info">
-		        <div class="teach-title">
-                            产品说明
-                        </div>
-			<div>
-				
-产品型号	
-HS1GW智能网关
-
-工作电压	AC100V~240V
-平均功耗	≤0.5W
-报警电流	≤30mA
-联网方式	ZigBee自组网
-无线组网距离 	≤100米(空旷环境)
-工作温度	-10℃~+50℃
-环境湿度	最大95%RH
-A 外形尺寸	122*122*52mm
-B 产品尺寸	70*70*53mm
-			</div>
-		    </div>
-                </div>
-            </div>
-</div>
+    <tr>
+        <td>环境湿度	</td>
+        <td>最大95%RH</td>
+        <td>探测器尺寸		</td>
+        <td>60*30*11.8mm</td>
+    </tr>
+    <tr>
+        <td>磁体尺寸		</td>
+        <td>65*13*12.5mm</td>
+        <td></td>
+        <td></td>
+    </tr>
+</table> 
+     </div>
+     </div> 
+    </div> 
+   </div> 
+  </div>
 </template>
 
 <script>
